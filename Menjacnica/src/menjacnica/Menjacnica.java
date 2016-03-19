85830ac57225c17929c877b5b19530cc6e0e849c
popovic-marko <popovic.marko14@gmail.com>
2016-03-19 07:01:41 +0100

    U klasi Menjacnica implementirana metoda dodajKurs

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,15 +1,35 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	private LinkedList<Valuta> valute;
+	
+	
 	@Override
 	public void dodajKurs(String naziv, String skraceniNaziv, GregorianCalendar datum, int prodajni, int srednji,
 			int kupovni) {
-
+		boolean unet = false;
+		Kurs k = new Kurs(datum, prodajni, kupovni, srednji);
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getNaziv().equals(naziv)){
+				LinkedList<Kurs> lista = valute.get(i).getKursevi();
+				lista.add(k);
+				valute.get(i).setKursevi(lista);
+				unet = true;
+				break;
+			}
+		}
+		if(!unet){
+			LinkedList<Kurs> lista = new LinkedList<>();
+			lista.add(k);
+			Valuta v = new Valuta(naziv, skraceniNaziv, lista);
+			valute.add(v);
+		}
 	}
 
 	@Override
